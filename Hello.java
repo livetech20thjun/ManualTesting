112e1326bf01801df23e7bee0c9ed253e17f7682
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-07-19 08:58:52 +0530

    Java Season Notes and Programs till july 19th

---
+++ Hello.java
@@ -0,0 +1,33 @@
+// this is a java program to print Hello java on console
+/* 
+this is developed by Riyaz
+developed on 19th july
+reviewed by Prudhvi
+reviewed date 19th july
+*/
+
+//import java.util.*;
+import java.util.Scanner;
+
+class Hello
+{
+
+static int x;
+
+public static void main(String[] args)
+{
+
+System.out.println("Hello ...Java ...");
+System.out.println(x);
+
+Scanner input=new Scanner(System.in);
+int y=input.nextInt();
+System.out.println(y);
+
+
+
+
+}
+
+}
+//end of program
