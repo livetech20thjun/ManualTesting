7f0c1bdcd3c96862881d847572b8d5d230d1d46f
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-07-23 08:49:53 +0530

    Java Season Notes and Programs till july 23rd

---
+++ VariablesDemo1.java
@@ -0,0 +1,27 @@
+class VariablesDemo1
+{
+
+public static void main(String[] args)
+{
+
+int x=10;
+
+int b=0;
+
+System.out.println(x);
+
+System.out.println(b);
+
+}
+
+public void m1()
+{
+int a=100;
+
+System.out.println(a);
+
+
+}
+
+
+}
